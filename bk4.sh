9eef483d455b2d43f5aa1bd1b7776793f8e12c81
daemon-r7 <daemon@coremedia-solution.com>
2017-08-13 13:38:11 +0200

    update

--- bk4.sh
+++ bk4.sh
@@ -10,9 +10,9 @@
 # bug or anything: d43M0n23@3xpl0it.com
 ######################################################################
 # TODO EXAMPLE:
-# ??
-# script soll den www ordner auslesen und jeden order ausser 
-# BLB backupen sowie einen mysql dump erstellen von allen DBs
+# create a *.bk4 file in the directory 4 backup-job
+# like /home/dave/index.bk4 /home/mike/index.bk4
+# the script make only a backup from directorys with bk4 files include
 ######################################################################
 
 ##Variablen
@@ -26,11 +26,6 @@ DESTINATION=/full/path
 DATABASE_USER='root'
 DATABASE_PASSWORD=$0
 DATABASE_HOST='localhost'
-
-#Variablen
-owntag=core
-mp4tag=mp4
-mkvtag=mkv
 
 # Tools install
 #if ! hash exiftool 2>/dev/null; then sudo apt-get update && apt-get upgrade -y; sudo apt-get install --yes exiftool ; fi
@@ -47,16 +42,17 @@ do
         DirPath="$(dirname $DirPath)"
         DirPath="$(basename $DirPath)"
         echo "$DirPath"
-#        newtag=${DirPath%.XXX*}
+	dirname=$DirPath
 #        newfile=${file%.*}
-#        ffmpeg -i $file -c copy -metadata title="$newtag" -metadata comment="$newtag" -y ${newfile}_${owntag}.${mp4tag}
-        echo $file gefunden.
-#        rm $file
+#        echo $file gefunden. <- BK4 FILE
+
+	# Make The Weekly Backup
+	tar -zcvf ${dirname}_${DATE}.tar.gz $dirname
+	#mysqldump -h $DATABASE_HOST -u $DATABASE_USER -p$DATABASE_PASSWORD $DATABASE > `dirname $0`/${DAY_OF_WEEK}.sql
+	#tar -zcvf `dirname $0`/tmp/weekly/${DAY_OF_WEEK}_database.tar.gz `dirname $0`/${DAY_OF_WEEK}.sql
+	#rm -rf `dirname $0`/${DAY_OF_WEEK}.sql
+
+echo 'Made $dirname weekly backup...'
   done
-#  find "$file" -type f -not -name ".*" | grep .mp4$ | while read file
-#  do
-#        newfile=${file%_core.*}
-#        mv $file ${newfile}.${mp4tag}
-#        echo $file verschoben.
-#   done
+#  find  bla bla 
 done

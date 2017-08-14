1cd2877f7a74ecc0736f779600a98d636f362118
daemon-r7 <daemon@coremedia-solution.com>
2017-08-14 11:48:38 +0200

    update

--- bk4.sh
+++ bk4.sh
@@ -14,6 +14,33 @@
 # like /home/dave/index.bk4 /home/mike/index.bk4
 # the script make only a backup from directorys with bk4 files include
 ######################################################################
+#
+######################################################################
+# Bash sTyl3!
+clear='\033[0m'			#alle Attribute zurücksetzen
+#red='\e[31m'
+bold='\033[1m'			#Fettschrift
+underline='\033[4m'		#Unterstreichen
+blinken='\033[5m'		#Blinken
+invers='\033[7m'		#inverse Darstellung
+black='\033[30m'		#Schriftfarbe schwarz
+red='\033[31m'			#Schriftfarbe rot
+green='\033[32m'		#Schriftfarbe grün
+yell='\033[33m'			#Schriftfarbe gelb
+blue='\033[34m'			#Schriftfarbe blau
+mag='\033[35m'			#Schriftfarbe magenta
+turk='\033[36m'			#Schriftfarbe türkis
+white='\033[37m'		#Schriftfarbe weiß
+## B4ckgr0unD
+bgblack='\033[40m'		#Hintergrund schwarz
+bgred='\033[41m'		#Hintergrund rot
+bggreen='\033[42m'		#Hintergrund grün
+bgyell='\033[43m'		#Hintergrund gelb
+bgblue='\033[44m'		#Hintergrund blau
+bgmag='\033[45m'		#Hintergrund magenta
+bgturk='\033[46m'		#Hintergrund türkis
+bgwhite='\033[47m'		#Hintergrund weiß
+#######################################################################
 
 ##Variablen
 DATE=$(date +%F)
@@ -42,7 +69,7 @@ do
         DirPath="$(basename $DirPath)"
 	bk4dir=$DirPath
 	#newfile=${file%.*}
-	echo $DirPath
+	echo -e "\n${yell}Backup von $DirPath wird erstellt..${clear}"
 
 	# Make The Weekly Backup
 	tar -zcvf ${bk4dir}_${DATE}.tar.gz $bk4dir
@@ -52,14 +79,14 @@ do
 
 	mysqldump -h $DATABASE_HOST -u $DATABASE_USER -p$DATABASE_PASSWORD $DATABASE > ${bk4dir}_${DATE}.sql
 	if [ -f ${bk4dir}_${DATE}.sql ]; then
-	echo "${bk4dir}_${DATE}.sql erstellt"
+	echo -e "\n${bk4dir}_${DATE}.sql erstellt"
 	tar -zcvf ${bk4dir}_${DATE}.sql.tar.gz ${bk4dir}_${DATE}.sql
 	#rm ${bk4dir}_${DATE}.sql
 	else
-	echo "Fehler bei SQL erstellung!"
+	echo -e "\nFehler bei SQL erstellung!"
 	fi
 
-echo "Made ${bk4dir} weekly backup..."
+echo "${red}Made ${bk4dir} weekly backup...${clear}"
   done
 #find
 done
